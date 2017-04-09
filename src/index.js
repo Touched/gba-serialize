d649c2246842016ab1fe389963a7b8c4f1650324
Touched <jtouched@gmail.com>
2017-04-09 20:05:33 +0200

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,24 @@
+/* @flow */
+
+export { default as ArraySchema } from './schema/array';
+export { default as BitfieldSchema } from './schema/bitfield';
+export { default as BooleanSchema } from './schema/bool';
+export { default as ColorSchema, PaletteSchema } from './schema/color';
+export { default as CompressionSchema } from './schema/compression';
+export { default as EnumSchema } from './schema/enum';
+export { default as ImageSchema } from './schema/image';
+export {
+  default as IntegerSchema,
+  Word,
+  SignedWord,
+  HalfWord,
+  SignedHalfWord,
+  Byte,
+  SignedByte,
+} from './schema/integer';
+export { default as NamedValueSchema } from './schema/namedValue';
+export { default as PointerSchema } from './schema/pointer';
+export { default as Schema } from './schema/schema';
+export { default as StructureSchema } from './schema/structure';
+export { default as TupleSchema } from './schema/tuple';
+export { default as addressToOffset } from './util/addressToOffset';
