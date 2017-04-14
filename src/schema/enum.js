74806ba4ff34700a9f5ba5a63e2143117ae9a565
Touched <jtouched@gmail.com>
2017-04-14 15:29:35 +0200

    Strict enums

--- src/schema/enum.js
+++ src/schema/enum.js
@@ -7,16 +7,28 @@ import IntegerSchema from './integer';
 export default class EnumSchema extends Schema<?string> {
   backingSchema: IntegerSchema;
   mapping: Map<number, string>;
+  strict: boolean;
 
-  constructor(mapping: Map<number, string>, backingSchema: IntegerSchema) {
+  constructor(mapping: Map<number, string>, backingSchema: IntegerSchema, strict: boolean = false) {
     super();
     this.backingSchema = backingSchema;
     this.mapping = mapping;
+    this.strict = strict;
   }
 
   unpack(buffer: Buffer, offset: number = 0, context: Context = new Context()): ?string {
     const value = this.backingSchema.unpack(buffer, offset, context);
-    return this.mapping.get(value) || null;
+    const name = this.mapping.get(value);
+
+    if (name) {
+      return name;
+    }
+
+    if (this.strict) {
+      throw new Error(`Invalid value for enum: ${value}`);
+    }
+
+    return null;
   }
 
   size(): number {
